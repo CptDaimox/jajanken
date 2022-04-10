6553a2ee8c61b9841d2efc16a5f06652e2f5ea11
Toan <toan.ttq@gmx.de>
2022-04-10 17:58:21 +0200

    add game ui

--- scripts/jajanken.js
+++ scripts/jajanken.js
@@ -1,4 +1,59 @@
 const jankenSet = ["Rock", "Paper", "Scissors"];
+let score = { lose: 0, win: 0, tie: 0 };
+const resultEmoji = { lost: "🙊", won: "🤌" };
+const buttons = document.querySelectorAll(".btn");
+const playerScore = document.querySelector("#player");
+const computerScore = document.querySelector("#computer");
+const winText = document.querySelector("h2");
+const resultText = document.querySelector("h3");
+const resetBtn = createResteBtn();
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (!isGameOver()) {
+      let computer = computerPlay();
+      let player = convertChoice(button.id);
+
+      result = playRound(computer, player);
+      score[result]++;
+
+      textResult = createResultText(result, computer, player).split("!");
+      winText.textContent = textResult[0];
+      resultText.textContent = textResult[1];
+      playerScore.textContent = `🐵: ${score["win"]}`;
+      computerScore.textContent = `🤖: ${score["lose"]}`;
+    }
+
+    if (isGameOver()) endGame();
+  });
+});
+
+function isGameOver() {
+  return score["lose"] == 5 || score["win"] == 5;
+}
+
+function endGame() {
+  score.end = score.lose > score.win ? "lost" : "won";
+  winText.textContent = `You ${score.end} the game ${resultEmoji[score.end]}`;
+  resultText.replaceWith(resetBtn);
+}
+
+function resetGame() {
+  score = { lose: 0, win: 0, tie: 0 };
+  resetBtn.replaceWith(resultText);
+  winText.textContent = "Choose wisely!";
+  playerScore.textContent = `🐵: 0`;
+  computerScore.textContent = `🤖: 0`;
+}
+
+function createResteBtn() {
+  btn = document.createElement("button");
+  btn.setAttribute("id", "reset");
+  btn.addEventListener("click", () => resetGame());
+  btn.textContent = "Play again";
+  btn.addEventListener("click", resetGame);
+  return btn;
+}
 
 function computerPlay() {
   return Math.floor(Math.random() * 3);
@@ -7,11 +62,11 @@ function computerPlay() {
 function playRound(computer, player) {
   diff = player - computer;
   // Win
-  if (diff == 1 || diff == -2) return 1;
+  if (diff == 1 || diff == -2) return "win";
   // Lose
-  else if (diff == 2 || diff == -1) return 0;
+  else if (diff == 2 || diff == -1) return "lose";
   // Tie
-  else return 2;
+  else return "tie";
 }
 
 function game() {
@@ -33,8 +88,6 @@ function game() {
   else console.log("You lost!");
 }
 
-console.log(game());
-
 function convertChoice(choice) {
   choice = choice.toLowerCase();
   switch (choice) {
@@ -47,14 +100,13 @@ function convertChoice(choice) {
   }
 }
 
-function createResultText(computer, player) {
-  let result = playRound(computer, player);
+function createResultText(result, computer, player) {
   switch (result) {
-    case 0:
+    case "lose":
       return `You Lose! ${jankenSet[computer]} beats ${jankenSet[player]}`;
-    case 1:
+    case "win":
       return `You Win! ${jankenSet[player]} beats ${jankenSet[computer]}`;
-    case 2:
-      return "It's a tie!";
+    case "tie":
+      return "It's a tie! Can't beat fire with fire";
   }
 }
